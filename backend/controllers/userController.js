d6dc06f2ca58cbca4ee90ebd98f1a69864373ed8
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-14 21:56:15 +0530

    Custom Error Middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,9 +1,11 @@
+import asyncHandler from "express-async-handler";
+
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access public
 
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "authUsers" });
-};
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,9 @@ import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
+// import second from 'first'
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
 const PORT = process.env.PORT || 5000;
 const app = express();
 
@@ -9,4 +12,6 @@ app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => res.send("server is ready"));
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`server started in `, PORT));
