3f43e44c7b101d1b8777e512a6f2516e5fb3a499
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-15 08:17:56 +0530

    MongoDB Database Setup

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
+import connectDB from "./config/db.js";
 dotenv.config();
 // import second from 'first'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
-
+connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
 
