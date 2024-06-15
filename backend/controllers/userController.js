cf9627d0883ea719f26cb90dfba7295ba1ea477b
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-15 07:59:04 +0530

    Connect to User Routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -8,4 +8,41 @@ const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "authUsers" });
 });
 
-export { authUser };
+// @desc Register a new user
+// route POST /api/users/
+// @access public
+
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "registerUsers" });
+});
+
+// @desc Logout user
+// route POST /api/users/logout
+// @access public
+
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "logoutUsers" });
+});
+// @desc get user  profile
+// route Get /api/users/profile
+// @access private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "userProfile" });
+});
+
+// @desc update user Profile
+// route Put /api/users/profile
+// @access private
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "updateUserProfile" });
+});
+
+export {
+  authUser,
+  logoutUser,
+  registerUser,
+  getUserProfile,
+  updateUserProfile,
+};
