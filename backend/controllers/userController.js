c869d01649e1b76569c4f049c3feaa5a89a2dfc4
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-15 10:38:08 +0530

    Auth User Endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,7 +6,16 @@ import generateToken from "../utils/generateToken.js";
 // @access public
 
 const authUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "authUsers" });
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+  } else {
+    res.status(401);
+    throw new Error("Invalid User Email or Password");
+  }
 });
 
 // @desc Register a new user
