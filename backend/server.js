f9d357d2327c51cebad85c36e8f32f74cd0362a5
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-15 08:56:26 +0530

    Register User Endpoint

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,14 @@ import dotenv from "dotenv";
 import userRoutes from "./routes/userRoutes.js";
 import connectDB from "./config/db.js";
 dotenv.config();
+
 // import second from 'first'
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 connectDB();
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/users", userRoutes);
 
